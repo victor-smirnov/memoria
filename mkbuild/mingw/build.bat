83d8670c9ddc773cee9d91b6bd3fd74db4d601b7
Ivan Yurchenko <ivan0yurchenko@gmail.com>
2011-10-30 23:36:56 +0400

    Powershell construction for getting time replaced with simple echo.

--- mkbuild/mingw/build.bat
+++ mkbuild/mingw/build.bat
@@ -1,6 +1,8 @@
 @echo off
 call env.bat
 
-powershell -Command "&{Get-Date}" >build_time.txt
-mingw32-make 2>build_log.txt
-powershell -Command "&{Get-Date}" >>build_time.txt
+echo %time% > build_time.txt
+
+mingw32-make 2> build_log.txt
+
+echo %time% >> build_time.txt
