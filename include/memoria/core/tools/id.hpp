61cbade945b40b90d7461b2ea4af61630dc5091c
Ivan Yurchenko <ivan0yurchenko@gmail.com>
2011-10-31 11:06:06 +0400

    Heap of cases replaced with array.

--- include/memoria/core/tools/id.hpp
+++ include/memoria/core/tools/id.hpp
@@ -1,5 +1,5 @@
 
-// Copyright Victor Smirnov 2011.
+// Copyright Victor Smirnov, Ivan Yurchenko 2011.
 // Distributed under the Boost Software License, Version 1.0.
 // (See accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
@@ -124,24 +124,14 @@ public:
 private:
 
     static char get_char(Byte value) {
-        switch (value) {
-            case 0: return '0';
-            case 1: return '1';
-            case 2: return '2';
-            case 3: return '3';
-            case 4: return '4';
-            case 5: return '5';
-            case 6: return '6';
-            case 7: return '7';
-            case 8: return '8';
-            case 9: return '9';
-            case 10: return 'a';
-            case 11: return 'b';
-            case 12: return 'c';
-            case 13: return 'd';
-            case 14: return 'e';
-            case 15: return 'f';
-        }
+    	char chars[] = {
+       		'0', '1', '2', '3',
+       		'4', '5', '6', '7',
+       		'8', '9', 'a', 'b',
+       		'c', 'd', 'e', 'f'};
+    	if (value >= 0 && value <= 15) {
+    		return chars[value];
+    	}
         return 'X';
     }
 };
