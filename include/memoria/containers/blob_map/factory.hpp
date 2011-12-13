ec3850557a7872fe0e05faba9cb83defe031da09
Victor Smirnov <aist11@gmail.com>
2011-12-13 15:16:07 +0300

    files...

---
+++ include/memoria/containers/blob_map/factory.hpp
@@ -0,0 +1,43 @@
+
+// Copyright Victor Smirnov, Ivan Yurchenko 2011.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+
+
+#ifndef _MEMORIA_MODELS_ROOT_FACTORY_HPP
+#define _MEMORIA_MODELS_ROOT_FACTORY_HPP
+
+
+#include <memoria/containers/root/container/container.hpp>
+
+namespace memoria {
+
+template <typename Types>
+struct BlobMapCtrTypes: Types {};
+
+template <typename Profile_, typename T>
+class CtrTF<Profile_, BlobMap, T> {
+
+	typedef CtrTF<Profile_, BlobMap, T> 										MyType;
+
+	typedef typename ContainerCollectionCfg<Profile_>::Types::AbstractAllocator	Allocator;
+
+public:
+
+	struct Types {
+		typedef Profile_					Profile;
+		typedef MyType::Allocator			Allocator;
+
+		typedef BlobMapCtrTypes<Types>		CtrTypes;
+	};
+
+	typedef typename Types::CtrTypes 											CtrTypes;
+	typedef Ctr<CtrTypes>														Type;
+};
+
+
+}
+
+#endif
