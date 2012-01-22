65af6d6a542026059fa0a9fd605650ba4978f35e
Ivan Yurchenko <ivan0yurchenko@gmail.com>
2012-01-23 01:47:21 +0400

    Build scripts have been improved. mkbuild.sh has been made common. Common script files directory has been renamed.

--- mkbuild/common/mkbuild.sh.tpl
+++ mkbuild/common/mkbuild.sh.tpl
@@ -23,14 +23,18 @@ fi
 BASE_DIR=$(dirname $0)
 CURRENT_DIR=`pwd`
 
+# placeholders will be replaced with sed's regexp
+C_COMPILER=`which @@C_COMPILER_PLACEHOLDER@@`
+CXX_COMPILER=`which @@CXX_COMPILER_PLACEHOLDER@@`
+
 # if you know a better way to specify out-of-source output directory,
 # please email me on ivanhoe12@gmail.com <Ivan>
 cd $BASE_DIR
 
 cmake -G "Unix Makefiles" \
     -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_C_COMPILER=/usr/bin/clang \
-    -DCMAKE_CXX_COMPILER=/usr/bin/clang++ \
+    -DCMAKE_C_COMPILER=$C_COMPILER \
+    -DCMAKE_CXX_COMPILER=$CXX_COMPILER \
     -DMEMORIA_LINK_FLAGS="-lpthread -L/usr/local/lib" \
     -DBUILD_TOOLS=true \
     -DMEMORIA_LIBS=libc++.a \
---
+++ mkbuild/setup.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+print_usage()
+{
+    SCRIPT_NAME="setup.sh"
+    echo "USAGE: $SCRIPT_NAME [--gcc|--clang]"
+    echo "Default is --gcc"
+}
+
+# default is gcc
+C_COMPILER="gcc"
+CXX_COMPILER="g++"
+BUILD_DIR="unix"
+
+if [ $# -eq 0 ] ; then : # do nothig
+elif [ $# -eq 1 ]; then
+    case $1 in
+        --gcc)
+            ;; # do nothig
+        --clang)
+            C_COMPILER="clang"
+            CXX_COMPILER="clang++"
+            BUILD_DIR="clang"
+            ;;
+        *)
+            print_usage
+            exit 1
+            ;;
+    esac
+else
+    print_usage
+    exit 1
+fi
+
+BASE_DIR=$(dirname $0)
+BUILD_DIR=$BASE_DIR/../../memoria-build/$BUILD_DIR
+COMMON_DIR=$BASE_DIR/_common
+
+mkdir -p $BUILD_DIR
+
+cp -i $COMMON_DIR/build.sh.tpl $BUILD_DIR/build.sh
+cp -i $COMMON_DIR/clean.sh.tpl $BUILD_DIR/clean.sh
+
+sed -e "s/@@C_COMPILER_PLACEHOLDER@@/${C_COMPILER}/" \
+    -e "s/@@CXX_COMPILER_PLACEHOLDER@@/${CXX_COMPILER}/" \
+     $COMMON_DIR/mkbuild.sh.tpl > $BUILD_DIR/mkbuild.sh
+chmod +x $BUILD_DIR/mkbuild.sh
