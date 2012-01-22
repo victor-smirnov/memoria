2c455b53426e8d7ec0508ca7fe9d4d2f52b52e9f
Ivan Yurchenko <ivan0yurchenko@gmail.com>
2012-01-23 00:11:37 +0400

    Common script files directory has been renamed. Some script files have been made common.

--- mkbuild/clang/setup.sh
+++ mkbuild/clang/setup.sh
@@ -2,9 +2,10 @@
 
 BASE_DIR=$(dirname $0)
 BUILD_DIR=$BASE_DIR/../../../memoria-build/clang
+COMMON_DIR=$BASE_DIR/../_common
 
 mkdir -p $BUILD_DIR
 
-cp -i $BASE_DIR/build.sh		$BUILD_DIR
-cp -i $BASE_DIR/clean.sh		$BUILD_DIR
+cp -i $COMMON_DIR/build.sh.tpl	$BUILD_DIR/build.sh
+cp -i $COMMON_DIR/clean.sh.tpl	$BUILD_DIR/clean.sh
 cp -i $BASE_DIR/mkbuild.sh		$BUILD_DIR
--- mkbuild/unix/clean.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-BASE_DIR=$(dirname $0)
-
-make --no-print-directory -C $BASE_DIR clean
