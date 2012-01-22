a500db7dbdc48c7634925501b196c75b759722f6
Ivan Yurchenko <ivan0yurchenko@gmail.com>
2012-01-22 20:58:56 +0400

    Setup scripts (setup.sh and setup.bat) now works not only from current directory. Thus, issue #7 is resolved.

--- mkbuild/msvc/setup.bat
+++ mkbuild/msvc/setup.bat
@@ -1,8 +1,10 @@
 @ECHO OFF
 
-SET BUILD=..\..\..\memoria-build\msvc
+SET BASE_DIR=%~dp0
+SET BASE_DIR=%BASE_DIR:~0,-`%
+SET BUILD_DIR=%BASE_DIR%\..\..\..\memoria-build\msvc
 
-MKDIR %BUILD%
+MKDIR %BUILD_DIR%
 
-COPY /-Y *.bat %BUILD%
-DEL %BUILD%\setup.bat
+COPY /-Y %BASE_DIR%\*.bat %BUILD_DIR%
+DEL %BUILD_DIR%\setup.bat
--- mkbuild/unix/setup.sh
+++ mkbuild/unix/setup.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-BUILD=../../../memoria-build/unix
+BASE_DIR=$(dirname $0)
+BUILD_DIR=$BASE_DIR/../../../memoria-build/unix
 
-mkdir -p $BUILD
+mkdir -p $BUILD_DIR
 
-cp -i build.sh 		$BUILD
-cp -i clean.sh 		$BUILD
-cp -i mkbuild.sh	$BUILD
+cp -i $BASE_DIR/build.sh		$BUILD_DIR
+cp -i $BASE_DIR/clean.sh		$BUILD_DIR
+cp -i $BASE_DIR/mkbuild.sh		$BUILD_DIR
