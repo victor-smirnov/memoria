020d66b52f66fe61985a58047c9e8eaaee8c0708
Victor Smirnov <aist11@gmail.com>
2018-06-11 03:28:39 +0300

    Integer intrinsics for msvc

--- include/memoria/v1/core/integer/msvc/integer_primitives.hpp
+++ include/memoria/v1/core/integer/msvc/integer_primitives.hpp
@@ -18,6 +18,8 @@
 #include <memoria/v1/core/types.hpp>
 
 #include <iostream>
+#include <intrin.h>
+
 
 namespace memoria {
 namespace v1 {
@@ -29,29 +31,38 @@ template <typename V> struct BuiltinSubCH;
 
 template <>
 struct BuiltinAddCH<unsigned long> {
-    static unsigned long process(unsigned long x, unsigned long y, unsigned long carry_in, unsigned long& carry_out) {
-        return 0;//__builtin_addcl(x, y, carry_in, &carry_out);
+    static unsigned long process(unsigned long x, unsigned long y, unsigned long carry_in, unsigned long& carry_out) 
+	{
+		unsigned __int32 res;
+		carry_out = _addcarry_u32(carry_in, x, y, &res);
+		return res;
     }
 };
 
 template <>
 struct BuiltinAddCH<unsigned long long> {
     static unsigned long long process(unsigned long long x, unsigned long long y, unsigned long long carry_in, unsigned long long& carry_out) {
-        return 0;//__builtin_addcll(x, y, carry_in, &carry_out);
+		unsigned __int64 res;
+		carry_out = _addcarry_u64(carry_in, x, y, &res);
+		return res;
     }
 };
 
 template <>
 struct BuiltinSubCH<unsigned long> {
     static unsigned long process(unsigned long x, unsigned long y, unsigned long carry_in, unsigned long& carry_out) {
-        return 0;//__builtin_subcl(x, y, carry_in, &carry_out);
+		unsigned __int32 res;
+		carry_out = _subborrow_u32(carry_in, x, y, &res);
+		return res;
     }
 };
 
 template <>
 struct BuiltinSubCH<unsigned long long> {
     static unsigned long long process(unsigned long long x, unsigned long long y, unsigned long long carry_in, unsigned long long& carry_out) {
-        return 0;//__builtin_subcll(x, y, carry_in, &carry_out);
+		unsigned __int64 res;
+		carry_out = _subborrow_u64(carry_in, x, y, &res);
+		return res;
     }
 };
 
