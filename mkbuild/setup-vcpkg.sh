082c2912bf76e1ceb91508b5eab556bd3bf4f595
Victor Smirnov <aist11@gmail.com>
2020-01-22 04:51:50 +0300

    MSVC build cleanup

--- mkbuild/setup-vcpkg.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mkdir memoria-build
-cd memoria-build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_C_COMPILER=clang -DCMAKE_TOOLCHAIN_FILE=../vcpkg-memoria/scripts/buildsystems/vcpkg.cmake  -DBUILD_TESTS=ON  ../memoria
